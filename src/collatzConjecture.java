2f10498a7fa7e3bd0253d5b4f86ee7afcd04e8b4
Ahmad Sadid Wafiq <101873162+Wafiq5@users.noreply.github.com>
2024-12-06 10:49:42 -0600

    Update collatzConjecture.java

--- src/collatzConjecture.java
+++ src/collatzConjecture.java
@@ -1,9 +1,10 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.Scanner;
 
 public class collatzConjecture {
     public static ArrayList<BigInteger> findSequence(BigInteger numParam){
+
         ArrayList<BigInteger> sequence = new ArrayList<>();
         BigInteger temp = numParam;
         BigInteger zero = BigInteger.ZERO;
@@ -27,3 +28,35 @@ public class collatzConjecture {
         return sequence;
     }
 }
+
+
+//! MAIN METHOD
+
+//Scanner ask = new Scanner(System.in);
+//BigInteger number = ask.nextBigInteger();
+//BigInteger one = BigInteger.ONE;
+//BigInteger two = BigInteger.TWO;
+//BigInteger four = new BigInteger("4");
+//
+//
+//ArrayList<BigInteger> sequence = new ArrayList<BigInteger>();
+//sequence = collatzConjecture.findSequence(number);
+//long steps = + sequence.size();
+//BigInteger largest = BigInteger.ZERO;
+//        for (int i = 0; i < sequence.size(); i++) {
+//        if (sequence.get(i).compareTo(largest) > 0){
+//largest = sequence.get(i);
+//            }
+//                    }
+//boolean followsCollatzConjecture = true;
+//String followsCollatzConjectureString = "";
+//        if (sequence.get(sequence.size() - 3).equals(four) && sequence.get(sequence.size() - 2).equals(two) && sequence.get(sequence.size() - 1).equals(one)){
+//              followsCollatzConjectureString = "Yes";
+//        }else {
+//              followsCollatzConjectureString = "No";
+//        }
+//
+//        System.out.println(sequence);
+//        System.out.println("Steps: " + steps);
+//        System.out.println("Largest: " + largest);
+//        System.out.println("Follows Collatz Conjecture?: " + followsCollatzConjectureString);
